9977fcb5e60aefed42aa4695a6c18fac2281cc17
ssangam1024 <ssangam1024@gmail.com>
2025-10-23 14:07:33 -0600

    Added links to the hamburger icons

--- services.js
+++ services.js
@@ -28,3 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
     message.style.display = (message.style.display === "block") ? "none" : "block";
   }
   
+  function openPopup(event) {
+    event.preventDefault();
+    document.getElementById("popupOverlay").style.display = "flex";
+  }
+  
+  function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+  }
