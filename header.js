c7c5913c829c7590f17cff827fb64d010c03d27b
ssangam1024 <ssangam1024@gmail.com>
2025-11-04 12:34:06 -0700

    Created and updated a Common Header Component

--- header.js
+++ header.js
@@ -3,13 +3,10 @@ async function loadHeader() {
       const response = await fetch('header.html');
       if (!response.ok) throw new Error('Failed to load header');
       const headerHTML = await response.text();
-      
-      // Insert header at top of body
+
       document.body.insertAdjacentHTML('afterbegin', headerHTML);
     } catch (error) {
       console.error(error);
     }
   }
-  
-  // Load header after the DOM is ready
   document.addEventListener('DOMContentLoaded', loadHeader);
